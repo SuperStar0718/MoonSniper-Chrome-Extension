993efa1e2ce4400e068932e87f914a4fa3ff8f82
SuperStar0718 <georgejohnson11305@gmail.com>
2022-11-24 06:43:25 +0200

    initial chrome-app commit

---
+++ README.md
@@ -0,0 +1,30 @@
+![repository-banner.png](https://res.cloudinary.com/alvarosaburido/image/upload/v1612193118/as-portfolio/Repo_Banner_kexozw.png)
+
+# MoonSniper Chrome Application
+
+> This is Chrome Application for moonsniper.co 
+
+## Build Setup
+
+```bash
+# install dependencies
+$ yarn
+
+# build in dev mode
+$ yarn dev
+
+# build for production 
+$ yarn build
+```
+
+## Testing locally on Chrome
+
+In order to test the package holding the extension in Chrome we need to:
+1. Go to `chrome://extensions/` page.
+2. Toggle `Developer mode` ON.
+3. Select `Load Unpacked`.
+4. Select the `/dist` directory.
+
+We will publish "/dist" directory to google chrome app store!
+
+https://moonsniper.co
---
+++ src/content-scripts/main.js
@@ -0,0 +1,73 @@
+import { createApp } from "vue";
+import Popup from "./Popup.vue";
+import "@/styles/main.css";
+
+const MOUNT_EL_ID = "as-awesome-extension";
+
+let mountEl = document.getElementById(MOUNT_EL_ID);
+if (mountEl) {
+  mountEl.innerHTML = "";
+}
+mountEl = document.createElement("div");
+mountEl.setAttribute("id", MOUNT_EL_ID);
+document.body.appendChild(mountEl);
+
+const vm = createApp(Popup).mount(mountEl);
+
+/////////////////////////////
+
+const featureBoxRight = document.querySelectorAll('div.coin-table tbody tr');
+
+var option = document.createElement("td");
+option.classList = "table-td-moon"
+option.setAttribute("style","width: 53px;")
+if (featureBoxRight) {
+  for (let i = 0; i < featureBoxRight.length; i++) {
+
+    var symbol = featureBoxRight[i].querySelectorAll('a span')[1].innerHTML.trim();
+
+    option.innerHTML = '<span ><img style=" width:40px; cursor: pointer;" class="moon-logo" src="https://moonsniper.co/images/moon/icon/fullcolor.png"></span>';
+
+    featureBoxRight[i].prepend(option.cloneNode(true));
+
+    //Add data of symbol
+    var specificMoon = featureBoxRight[i].getElementsByTagName("span")[0];
+
+    specificMoon.addEventListener('click', triggerPopup, false);
+    specificMoon.symbol = symbol;
+  }
+}
+const body = document.querySelector('body');
+
+if (body) {
+  var div = document.createElement("div");
+  div.classList = "div-moon"
+  div.innerHTML = '<span class="click-modal"  data-toggle="modal" data-target="#myModal" style="display:none;" ></span>';
+
+  body.prepend(div.cloneNode(true));
+}
+
+/* fetch(chrome.runtime.getURL('/innerpage/template.html')).then(r => r.text()).then(html => {
+  document.body.insertAdjacentHTML('beforeend', html);
+  // not using innerHTML as it would break js event listeners of the page
+}); */
+
+
+
+
+function triggerPopup(event){
+  console.log("popup triggered with symbol: " + event.currentTarget.symbol);
+  vm.visible = !vm.visible;
+  /* let tdNode = event.target.parentNode.parentNode.parentNode;
+  let ReqTd = tdNode.querySelector('.cg-sticky-third-col');
+  document.querySelector(".modal").classList.add("show"); */
+}
+
+
+//////////////////////////////////////
+
+chrome.runtime.onMessage.addListener(message => {
+  if (message.toggleVisible) {
+    vm.visible = !vm.visible;
+  }
+});
